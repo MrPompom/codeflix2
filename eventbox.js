1bfe11db62b4942ce56e889b3b48604be1ed0c08
bastien prompsy <bastienprompsy@gmail.com>
2020-11-13 10:26:03 +0100

    second ex

--- eventbox.js
+++ eventbox.js
@@ -1,10 +1,17 @@
 function empty() {
     const EventEmitter =  require('event')
+    EventEmitter.on('hi', () => {
+        console.log("hi dear friends")
+    })
     EventEmitter.emit('hi')
 }
 
 function withArgs(names) {
-
+    const EventEmitter = require('event')
+    EventEmitter.on('newFeloow', names => {
+        console.log("Here comes's anew pirate ->>", names)
+    })
+    EventEmitter.emit('newFellow')
 }
 
 module.export = empty()
